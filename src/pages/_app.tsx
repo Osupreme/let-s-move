3692b2250fdf80bdbfbbea70666ec56fadf9fb16
unknown <gustavoprime81@gmail.com>
2021-02-28 17:06:31 -0300

    git commit src/pages/_app.tsx -m

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,8 @@
 import "../style/global.css";
 
-import { ChallengerProvider } from '../context/ChallegerContext'
-
 function MyApp({ Component, pageProps }) {
       return(
-          <ChallengerProvider>
           <Component {...pageProps} />
-          </ChallengerProvider>
       );
 }
 
