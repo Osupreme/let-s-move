80e1262c6b66ddae470192e56f0b703ab1d99d30
unknown <gustavoprime81@gmail.com>
2021-02-28 17:11:26 -0300

    added to index ChallengerProvider in html structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,37 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
 import { ChallengerBox } from '../componets/ChallengerBox';
 import { CompletteChallenge } from "../componets/CompleteChalleger";
 import { Countdown } from "../componets/CountDown";
 import { ExperienceBar } from "../componets/ExperenceBar";
 import { Profile } from "../componets/Profile";
 import { CountdownProvider } from '../context/CountdownContext';
+import { ChallengerProvider } from '../context/ChallegerContext';
+
 import styles from '../style/peges/Home.module.css';
 
-export default function Home() {
+
+interface Homepage{
+  level: number;
+  CurrentExperience: number;
+  ChallengerCompleted: number;
+}
+
+export default function Home(props: Homepage) {
   return (
+    <ChallengerProvider
+    level = {props.level}
+    ChallengerCompleted = {props.ChallengerCompleted}
+    CurrentExperience = {props.CurrentExperience}
+
+    >
     <div className={styles.container}>
       <Head>
         <title>Start | Move.it</title>
       </Head>
       <ExperienceBar />
-      
+
       <CountdownProvider>
       <section>
         <div>
@@ -29,5 +46,19 @@ export default function Home() {
       </section>
       </CountdownProvider>
     </div>
+    </ChallengerProvider>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, CurrentExperience, ChallengerCompleted } = ctx.req.cookies
+
+  return{
+    props: {
+      level: Number(level),
+      CurrentExperience: Number(CurrentExperience),
+      ChallengerCompleted: Number(ChallengerCompleted),
+    }
+  }
+
 }
